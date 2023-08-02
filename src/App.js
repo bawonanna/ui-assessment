2f1a7f9d200fd1e32100634196f70953125d8a6a
Bawo Nanna <bawo.nanna@gmail.com>
2023-08-02 14:55:47 -0500

    added tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import Display from "./components/Display";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function App() {
     const [finalPoints, setFinalPoints] = useState('')
@@ -11,10 +13,24 @@ function App() {
 
     return(
         <div>
-        <h1>Purchase Point Generator</h1>
+            <h1>Purchase Point Generator</h1>
+        <Tabs>
+                <TabList>
+                <Tab>Add Transaction</Tab>
+                <Tab>View History</Tab>
+                </TabList>
 
-         <Form onHandleForm={handleForm} />
-        {finalPoints && finalPoints !== '' ? <Display points={finalPoints} />: ("")}
+                <TabPanel>
+
+
+                    <Form onHandleForm={handleForm} />
+                    {finalPoints && finalPoints !== '' ? <Display points={finalPoints} />: ("")}
+                </TabPanel>
+                <TabPanel>
+                    {"History table here"}
+                </TabPanel>
+            </Tabs>
+        
         </div>
     );  
 };
